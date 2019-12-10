285638828020146f7be256824f474eeb0eca43eb
Ruanderson Vieira <ruanderson@accalia.io>
2019-12-10 09:16:14 -0300

    :sparkles: CRUD Projects

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [
+  {
+    id: "1",
+    title: "Projeto 1",
+    tasks: ["Tarefa 2", "Tarefa 2"]
+  },
+  {
+    id: "2",
+    title: "Projeto 2",
+    tasks: ["Tarefa 2", "Tarefa 2"]
+  }
+]; 
+
+server.use((req, res, next) => {
+  console.log(`Metodo: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.count(`Request`);
+});
+
+function checkProjectExists(req, res, next) {
+  if (!req.body.title) {
+    return res.status(400).json({ error: "Project title is required" });
+  }
+
+  return next();
+}
+
+function checkProjectInArray(req, res, next) {
+  const project = projects[req.params.id];
+
+  if (!project) {
+    return res.status(400).json({ error: "Project does not exists" });
+  }
+
+  req.project = project;
+
+  return next();
+}
+
+server.get('/projects', (req, res) => {
+  
+  return res.json(projects);
+});
+
+server.get('/projects/:id', checkProjectInArray, (req, res) => {
+  
+  return res.json(req.project);
+});
+
+server.post('/projects', checkProjectExists, (req, res) => {
+  const { id } = req.body;
+  const { title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(projects);
+});
+
+server.put('/projects/:id', checkProjectInArray, checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(project => project.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete('/projects/:id', checkProjectInArray, (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
+  return res.send();
+});
+
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(project => project.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3000);
